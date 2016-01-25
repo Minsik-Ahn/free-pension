2e81045f1fe89979286cda3c751771a210877733
Mins <minsik.ahn@worksmobile.com>
2016-01-25 17:27:25 +0900

    create error.html(error페이지 생성)

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ var users = require('./routes/users');
 var app = express();
 
 app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
 app.engine(".html", require("jade").renderFile);
 
-app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -51,7 +51,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render('error.html', {
     message: err.message,
     error: {}
   });
